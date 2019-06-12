cbb8c3a456d4fa8350556dbb66fb5b4c05e4fad5
tarunvelagala <tarunvelagala80@gmail.com>
2019-06-12 15:45:51 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  myData = [
+    {
+      name: "Person 1",
+      age: 20,
+      email: "tarunvelagala80@gmail.com"
+    },
+    {
+      name: "Person 2",
+      age: 21,
+      email: "tarunvelagala81@gmail.com"
+    },
+    {
+      name: "Person 3",
+      age: 22,
+      email: "tarunvelagala83@gmail.com"
+    }
+  ];
+  userForm;
+  constructor() {
+    this.userForm = new FormGroup({
+      'name': new FormControl(),
+      'age': new FormControl(),
+      'email': new FormControl()
+    })
+  };
+  formSubmit() {
+    this.myData.push(
+      this.userForm.value
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
